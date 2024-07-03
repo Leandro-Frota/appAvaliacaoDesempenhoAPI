1b9fb655261bb155961dcfaef3eb4ba16ccd37f4
Leandro-Frota <leandromfrota@gmail.com>
2024-07-03 17:05:41 -0300

    Add middlewate cors in app.js

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const rotaCadastro = require("./rotas/cadastro")
 
 const app = express();
 
-
 const PORT = 8000;
 
-app.use(express.json()) // middleware responsável por interpretar o body do req transformando json em objeto js
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.use(express.json()) // middleware responsável por interpretar o body do req transformando string em objeto js
 
 app.use('/cadastro', rotaCadastro )
-
-
 
 app.listen(PORT, ()=>{
     console.log(`Escutando  PORTA ${PORT}` )
